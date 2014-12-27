2b9dc038bfd628bcb0d3e39ad2ef009c01613fc8
Stuart Wheater <stuart.wheater@arjuna.com>
2014-12-27 23:58:11 +0000

    Started Tests

--- apachecamel-core/src/test/java/com/arjuna/dbplugins/tests/apachecamel/core/ApacheCamelDataProcessorTest.java
+++ apachecamel-core/src/test/java/com/arjuna/dbplugins/tests/apachecamel/core/ApacheCamelDataProcessorTest.java
@@ -4,17 +4,60 @@
 
 package com.arjuna.dbplugins.tests.apachecamel.core;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+
+import com.arjuna.databroker.data.connector.ObservableDataProvider;
+import com.arjuna.databroker.data.connector.ObserverDataConsumer;
+import com.arjuna.databroker.data.core.DataFlowNodeLifeCycleControl;
 import com.arjuna.dbplugins.apachecamel.core.ApacheCamelDataProcessor;
+import com.arjuna.dbutilities.testsupport.dataflownodes.dummy.DummyDataSink;
+import com.arjuna.dbutilities.testsupport.dataflownodes.dummy.DummyDataSource;
 
 public class ApacheCamelDataProcessorTest
 {
     private static final Logger logger = Logger.getLogger(ApacheCamelDataProcessorTest.class.getName());
 
     @Test
-    public void test01()
+    public void invocation01()
     {
+    	DataFlowNodeLifeCycleControl dataFlowNodeLifeCycleControl = null;
+
+        String              name       = "Apache Camel Data Processor";
+        Map<String, String> properties = new HashMap<String, String>();
+
+        DummyDataSource          dummyDataSource       = new DummyDataSource("Dummy Data Source", Collections.<String, String>emptyMap());
+        ApacheCamelDataProcessor apacheCamelDataSource = new ApacheCamelDataProcessor(name, properties);
+        DummyDataSink            dummyDataSink         = new DummyDataSink("Dummy Data Sink", Collections.<String, String>emptyMap());
+
+        dataFlowNodeLifeCycleControl.processCreatedDataFlowNode(dummyDataSource, null);
+        dataFlowNodeLifeCycleControl.processCreatedDataFlowNode(apacheCamelDataSource, null);
+        dataFlowNodeLifeCycleControl.processCreatedDataFlowNode(dummyDataSink, null);
+
+        ((ObservableDataProvider<String>) dummyDataSource.getDataProvider(String.class)).addDataConsumer((ObserverDataConsumer<String>) apacheCamelDataSource.getDataConsumer(String.class));
+        ((ObservableDataProvider<String>) apacheCamelDataSource.getDataProvider(String.class)).addDataConsumer((ObserverDataConsumer<String>) dummyDataSink.getDataConsumer(String.class));
+
+        dummyDataSource.sendData("Test");
+        
+        try
+        {
+            Thread.sleep(5000);
+        }
+        catch (InterruptedException interruptedException)
+        {
+            fail("Interrupted during sleep");
+        }
+
+        dataFlowNodeLifeCycleControl.removeDataFlowNode(dummyDataSource);
+        dataFlowNodeLifeCycleControl.removeDataFlowNode(apacheCamelDataSource);
+        dataFlowNodeLifeCycleControl.removeDataFlowNode(dummyDataSink);
+
+        assertTrue("Didn't receive any responce", dummyDataSink.receivedData().size() > 0);
     }
 }
